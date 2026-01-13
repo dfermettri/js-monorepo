3feae0a12fa772e851542d83c7f2fa2edd0ff424
dfermettri <234400449+dfermettri@users.noreply.github.com>
2026-01-13 23:53:09 +0600

    add new .js snippet

--- implement-the-slice-and-splice-algorithm.js
+++ implement-the-slice-and-splice-algorithm.js
@@ -5,7 +5,12 @@ function frankenSplice(arr,anotherArr,index) {
   return result;
 }
 
+//проверки
 console.log(frankenSplice([1, 2, 3], [4, 5], 1));
+//должен вернуть [4, 1, 2, 3, 5]
 console.log(frankenSplice([1, 2], ["a", "b"], 1));
+//должен вернуть ["a", 1, 2, "b"] 
 console.log(frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
+//должен вернуть ["head", "shoulders", "claw", "tentacle", "knees", "toes"]
 console.log(frankenSplice([1, 2, 3, 4], [], 0));
+//должен вернуть [1, 2, 3, 4]
