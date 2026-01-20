2f16870da0232be12e582c0e978eec98f1e761da
dfermettri <234400449+dfermettri@users.noreply.github.com>
2026-01-21 01:55:18 +0600

    add new .js snippet

--- inventory-management-program.js
+++ inventory-management-program.js
@@ -1,23 +1,61 @@
+//Начальный инвентарь (массив обьектов)
+
 let inventory = [];
+
+// Находит в массиве индекс товара по имени (игнорируя регистр);
+
 function findProductIndex (productName) {
-  if(inventory.name == productName) {
-    return inventory.quanity;
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].name.toLowerCase() == productName.toLowerCase()) {
+      return i;
+    }
   }
-  else return -1;
+  return -1;
 }
 
-function addProduct (product) {
-  if (product.name == inventory.name) {
-    product.name = product.name.toLowerCase();
-    inventory.quantity += product.quantity;
-    return `${product} quantity updated.`
+//Добавляет товар в инвентарь
+// или обновляет количество если он уже есть;
+function addProduct (productObj) {
+  productObj.name = productObj.name.toLowerCase()
+
+  let index = findProductIndex(productObj.name);
+  if (index !== -1) {
+    inventory[index].quantity += productObj.quantity;
+    return `${productObj.name} quantity updated`;
   }
   else {
-    inventory.push(product)
-    return `${product} added to inventory.`
+    inventory.push(productObj)
+    return `${productObj.name} added to inventory`;
   }
 }
 
-function removeProduct () {
+//Уменьшает количество товара,
+//либо удаляет его полностью,
+//либо выводит сообщение об ошибке;
+function removeProduct (productName, quantity) {
+  productName = productName.toLowerCase();
+  let index = findProductIndex(productName);
+  
+  if (index === -1) {
+    return `${productName} not found`;
+  }
+  else if (quantity > inventory[index].quantity) {
+    return `Not enough ${productName} available, remaining pieces: ${inventory[index].quantity}`;
+  }
+  else {
+    let remaining = inventory[index].quantity -= quantity;
 
+    if (inventory[index].quantity == 0) {
+      inventory.splice(index, 1);
+    }
+    return `Remaining ${productName} pieces: ${remaining}`;
+  }  
 }
+
+//тесты
+console.log(findProductIndex("flour"))
+console.log(findProductIndex("FloUr"))
+console.log(findProductIndex("Flour"))
+console.log(addProduct({name: "FLOUR", quantity: 5}))
+console.log(removeProduct("FLOUR", 5))
+console.log(removeProduct("FLOUR", 10))
