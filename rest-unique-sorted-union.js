786443bcad67721312b59824cea7bd98d48cef9c
dfermettri <234400449+dfermettri@users.noreply.github.com>
2026-01-22 20:01:44 +0600

    add new .js snippet

--- rest-unique-sorted-union.js
+++ rest-unique-sorted-union.js
@@ -1,6 +1,20 @@
 function uniteUnique (...arrs) {
-  let result = [];
+  let uniqueArr = [];
   for (const arr of arrs) {
-    
+    for (let index = 0; index < arr.length; index++) {
+      if (uniqueArr.includes(arr[index])) {
+        continue;
+      }
+      uniqueArr.push(arr[index]);
+    }
   }
+  return uniqueArr;
 }
+
+
+//tests
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
+console.log(uniteUnique([1, 2, 3], [5, 2, 1]))
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]))
+console.log(uniteUnique([1, 3, 2], [5, 4], [5, 6]))
+console.log(uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]))
